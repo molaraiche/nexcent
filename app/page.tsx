2c24d66f488222c6954bc6e687e894a2d30c85a6
molaraiche <laraichemohamed@gmail.com>
2024-09-04 16:28:24 +0100

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -20,12 +20,21 @@ const page = () => {
         <Community />
         <MoreDetails
           imgURL='./assets/phone-ill.svg'
-          title=' The unseen of spending three years at Pixelgrade'
+          title='The unseen of spending three years at Pixelgrade'
           details='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.'
         />
+      </div>
+      <div className='bg-neutral-Silver'>
         <div className='lg:container lg:mx-auto md:px-14 sm:px-10 xsm:px-4'>
           <Achievements />
         </div>
+      </div>
+      <div className='lg:container lg:mx-auto md:px-14 sm:px-10 xsm:px-4'>
+        <MoreDetails
+          imgURL='./assets/girl-ill.svg'
+          title='How to design your site footer like we did'
+          details='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.'
+        />
       </div>
     </main>
   );
